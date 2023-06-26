2b91cf522686d9d8825649bed84f1062ad135baf
Ghadi158 <ghadi1588@hotmail.com>
2023-06-26 18:45:43 +0300

    first commit

---
+++ src/getyear/GetYear.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package getyear;
+
+/**
+ *
+ * @author macbook
+ */
+public class GetYear {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //call frame here
+        CalculationFrame obj = new CalculationFrame();
+        obj.show();
+    }
+    
+}
